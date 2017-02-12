1a67b11d5f1ddf4403b5a5ec3acf78be65a901b4
venkat <kphani_ra@icloud.com>
2017-02-13 09:16:26 +1100

    checking in code

---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+
+    public class UserAuthentication : System.IdentityModel.Selectors.UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            try
+            {
+                if (userName == "test" && password == "test123")
+                {
+                    Console.WriteLine("Authentic User");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new FaultException("Unknown Username or Incorrect Password");
+            }
+        }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            client.ClientCredentials.UserName.UserName = "test";
+            client.ClientCredentials.ServiceCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+            client.GetData(10);
+
+            Console.ReadLine();
+        }
+    }
+}
